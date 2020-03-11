231907378c01decb7ffbce7290dd3dfbd276f122
Kevin PARIS <kparis@e2r6p2.42.fr>
2020-03-11 18:43:27 +0100

    wip

--- src/parse_cub_args.c
+++ src/parse_cub_args.c
@@ -6,7 +6,7 @@
 /*   By: kparis <kparis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 10:51:51 by kparis            #+#    #+#             */
-/*   Updated: 2020/03/11 17:28:04 by kparis           ###   ########.fr       */
+/*   Updated: 2020/03/11 18:08:46 by kparis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,22 @@ void	get_res(char **line_args, t_mlx *data)
 	data->map->res_y = ft_atoi(line_args[2]);
 }
 
+void	get_path(char **line_args, t_mlx *data)
+{
+	if (ft_str_tab_len(line_args) != 2)
+		error_message("wrong argument number in resolution field");
+	if (ft_strncmp(line_args[0], "NO", ft_strlen(line_args[0])) == 0)
+		data->map->n_path = ft_strdup(line_args[1]);
+	else if (ft_strncmp(line_args[0], "SO", ft_strlen(line_args[0])) == 0)
+		data->map->n_path = ft_strdup(line_args[1]);
+	else if (ft_strncmp(line_args[0], "WE", ft_strlen(line_args[0])) == 0)
+		data->map->n_path = ft_strdup(line_args[1]);
+	else if (ft_strncmp(line_args[0], "EA", ft_strlen(line_args[0])) == 0)
+		data->map->n_path = ft_strdup(line_args[1]);
+	else if (ft_strncmp(line_args[0], "S", ft_strlen(line_args[0])) == 0)
+		data->map->n_path = ft_strdup(line_args[1]);
+}
+
 void	parse_cub_args(t_mlx *data)
 {
 	int fd;
@@ -54,13 +70,15 @@ void	parse_cub_args(t_mlx *data)
 		line_args = ft_split(line, ' ');
 		//si
 			//resolution
-		if (line_args[0][0] == 'R')
+		if (ft_strncmp(line_args[0], "R", ft_strlen(line_args[0])) == 0)
 			get_res(line_args, data);
 			//textures
+			get_path(line_args, data);
 			//sol
 			//plafond
 		free(line);
 		parsed += 1;
 	}
 	close(fd);
+	ft_putendl(data->map->n_path);
 }
