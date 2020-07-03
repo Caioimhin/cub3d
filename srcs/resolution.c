9dd5e5e5089c7d71afdb80fee227e12283d7ec0a
Kévin PARIS <kevin.paris701@gmail.com>
2020-07-04 00:10:59 +0200

    WIP

--- srcs/resolution.c
+++ srcs/resolution.c
@@ -6,7 +6,7 @@
 /*   By: kparis <kparis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/13 18:58:37 by kparis            #+#    #+#             */
-/*   Updated: 2020/06/03 01:26:57 by kparis           ###   ########.fr       */
+/*   Updated: 2020/07/04 00:10:51 by kparis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,25 @@ void	check_res_values(t_data *data, t_maps *maps)
 
 void	get_resolution(t_data *data, char *line, t_maps *maps)
 {
-	int i;
+	int	i;
 
-	i = 0;
-	while (!ft_isdigit(line[i]))
-		i++;
-	maps->win_size.x = ft_atoi(&line[i]);
-	while (ft_isdigit(line[i]))
-		i++;
+	i = 1;
+	maps->win_size.x = -1;
+	maps->win_size.y = -1;
 	while (line[i] == ' ')
 		i++;
-	maps->win_size.y = ft_atoi(&line[i]);
 	while (ft_isdigit(line[i]))
+		maps->win_size.x = (maps->win_size.x * 10) + (line[i++] - '0');
+	while (line[i] == ' ')
 		i++;
+	while (ft_isdigit(line[i]))
+		maps->win_size.y = (maps->win_size.y * 10) + (line[i++] - '0');
 	while (line[i] == ' ')
 		i++;
 	if (line[i])
 		close_program(data, "Wrong resolution in .cub file\n", "");
 	if (maps->win_size.x < 50 || maps->win_size.y < 50)
 		close_program(data, "Resolution is too small\n", "");
+	if (maps->win_size.x == -1 || maps->win_size.y == -1)
+		close_program(data, "Resolution is not set\n", "");
 }
